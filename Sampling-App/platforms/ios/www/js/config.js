cd104dbc6bb320dec194e0897b4325a642597912
Marc <mmamarmarc@hotmail.com>
2018-02-12 17:55:12 +0100

    Cordova iOS platform

---
+++ Sampling-App/platforms/ios/www/js/config.js
@@ -0,0 +1,4 @@
+const USER_NAME = 'Marc';
+const PASSWORD = 'test123';
+const WEB_BASE = 'http://surveyhti.nfshost.com/survey/';
+const AUTORIZATION = "?user="+USER_NAME+"&pass="+PASSWORD;
---
+++ Sampling-App/platforms/ios/www/js/func.js
@@ -0,0 +1,80 @@
+function renderQuestion(Qid){
+	var render = true;
+	var date = new Date();
+	switch(Qid)
+	{
+		case "31":
+			//Example: only show question with id=3 in the weekend
+			var day = date.getDay();
+			if(day === 0 || day === 6) render = false;
+			else render = true;
+		break;
+	}
+	return render;
+}
+
+function autorizeUser()
+{
+	myApp.alert("check credentials");
+	user = $$("input#username").val();
+	pass = $$("input#password").val();
+	myApp.alert(WEB_BASE+"getUser.php"+AUTORIZATION);
+	$$.ajax({
+		url:WEB_BASE+"getUser.php"+AUTORIZATION+"&appUser="+user+"&appPass="+pass,
+		success: function(result){
+			var data = result.split("::");
+			if(data[0]==="true")
+			{
+				myApp.alert("login correct");
+				storage.setItem("Uid",data[1]);
+				storage.setItem("login","true");
+				view.router.loadPage('menu.html');
+			}
+			else
+			{
+				myApp.alert("login failed");
+			}
+		},
+		error(xhr,status,error){
+			myApp.alert("readyState: " + xhr.readyState);
+        	myApp.alert("responseText: "+ xhr.responseText);
+        	myApp.alert("status: " + xhr.status);
+        	myApp.alert("text status: " + status);
+        	myApp.alert("error: " + error);
+
+			//$$("#questions").html(status + " " + error);
+		}
+	});
+}
+
+function registerUser()
+{
+	user = $$("input#r_username").val();
+	pass = $$("input#r_password").val();
+	r_pass = $$("input#rr_password").val();
+	
+	if(user && pass && r_pass && pass === r_pass)
+	{
+		$$.ajax({
+			url:WEB_BASE+"createUser.php"+AUTORIZATION+"&appUser="+user+"&appPass="+pass,
+			success: function(result){
+				var data = result.split("::");
+				if(data[0]==="true")
+				{
+					storage.setItem("Uid",data[1]);
+					storage.setItem("login","true");
+					view.router.loadPage('menu.html');
+					myApp.alert("Registration correct");
+				}
+				else
+				{
+					myApp.alert("Username already exists, please choose another one.");
+				}
+			},
+			error(xhr,status,error){
+				myApp.alert('error data');
+				$$("#questions").html(status + " " + error);
+			}
+		});
+	}
+}
