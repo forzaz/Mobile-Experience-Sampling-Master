5b082add0d1df0bc651ae954b2852c91f2c88f6a
Chao Zhang <chao.zhang87@gmail.com>
2018-04-09 18:29:53 +0200

    With real information

--- Sampling-App/www/js/config.js
+++ Sampling-App/www/js/config.js
@@ -1,5 +1,5 @@
 /**
- * Experience Sampling App 1.0.1
+ * Experience Sampling App 1.0.0
  * This app allows researchers to conduct surveys remotely using the mobile phone on Android and iOS.
  * 
  * This app is developed by BOSONIC.design in assignment of the department 
@@ -8,16 +8,16 @@
  * info@bosonic.design || http://www.bosonic.design/
  * hti@tue.nl || https://www.tue.nl/en/university/departments/industrial-engineering-innovation-sciences/research/research-groups/human-technology-interaction/
  * 
- * Released on: April, 2018
+ * Released on: March, 2018
  */
 
 //KEEP THIS INFORMATION PRIVATE--------------------------------------------
 
 //Provide login details for the webinterface that is connected to the server.
 //This allows the app to retrieve information.
-const USER_NAME 	= 'app_user_name'; 			//server interface username
-const PASSWORD 		= 'app_user_password'; 		//server interface password
-const WEB_BASE 		= 'http://yourweb.com'; 	//link to where the server interface is located
+const USER_NAME 	= 'sampleu'; 			//server interface username
+const PASSWORD 		= 'Welcome@HTI'; 		//server interface password
+const WEB_BASE 		= 'https://htionline.tue.nl/0ccb706d/SampleU/Karin/'; 	//link to where the server interface is located
 const AUTORIZATION 	= "?user="+USER_NAME+"&pass="+PASSWORD; 	//<-- no need to change
 
 //CAMERA MODULE CONFIG
@@ -30,11 +30,3 @@ const PIC_HEIGHT 	= 200;	// Height of the picture saved on the server
 //Specify latitude and longitude of the maps initialization when the users GPS is turned off.
 const STANDARD_LAT = 51.44164199999999; //Current cordinates are set on Eindhoven, the Netherlands.
 const STANDARD_LNG = 5.469722499999989;
-
-
-// Initialize app variables
-var myApp = new Framework7();
-var storage = window.localStorage;
-var $$ = Dom7;
-var view = myApp.addView('.view-main', {dynamicNavbar: true, swipeback: false});
-var modules = [];
